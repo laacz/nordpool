d827c00a0b157441a8a96c59e3c8dce05bf1a3b7
Kaspars Foigts <laacz@laacz.lv>
2025-10-03 16:54:21 +0300

    chore: whoops

--- public/functions.php
+++ public/functions.php
@@ -301,3 +301,69 @@ class AppLocale
     }
 }
 
+class Cache {
+    const string DIR = __DIR__ . '/../cache/';
+
+    public static function get(string $key, mixed $default = null): mixed
+    {
+        $file = self::DIR . md5($key) . '.cache';
+        if (file_exists($file) && (time() - filemtime($file) < 3600)) {
+            return unserialize(file_get_contents($file));
+        }
+        return $default;
+    }
+
+    public static function set(string $key, mixed $value): void
+    {
+        $file = self::DIR . md5($key) . '.cache';
+        file_put_contents($file, serialize($value), LOCK_EX);
+    }
+
+    public static function delete(string $key): void
+    {
+        $file = self::DIR . md5($key) . '.cache';
+        if (file_exists($file)) {
+            unlink($file);
+        }
+    }
+
+    public static function clear(): void
+    {
+        $files = glob(self::DIR . '*.cache');
+        foreach ($files as $file) {
+            unlink($file);
+        }
+    }
+}
+
+class Lock {
+    const string DIR = __DIR__ . '/../cache/';
+
+    private string $file;
+    private $handle;
+
+    public function __construct(string $key)
+    {
+        $this->file = self::DIR . md5($key) . '.lock';
+        $this->handle = fopen($this->file, 'w+');
+    }
+
+    public function __destruct()
+    {
+        $this->unlock();
+        fclose($this->handle);
+        if (file_exists($this->file)) {
+            unlink($this->file);
+        }
+    }
+
+    public function lock(): bool
+    {
+        return flock($this->handle, LOCK_EX);
+    }
+
+    public function unlock(): bool
+    {
+        return flock($this->handle, LOCK_UN);
+    }
+}
